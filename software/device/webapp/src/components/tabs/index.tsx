246927b6046439d1af90ad2d06d1770c24cc84e5
Liv Fletcher <fletcholiv@ecs.vuw.ac.nz>
2023-05-10 10:37:33 +1200

    build(webapp): water history page, refers to the button for the history page within the main webapp ui

--- software/device/webapp/src/components/tabs/index.tsx
+++ software/device/webapp/src/components/tabs/index.tsx
@@ -36,6 +36,7 @@ const Tabs: React.FunctionComponent<TabProps> = ({ click, active, fullscreen }) 
                 <a className={style.button} onClick={() => click(0)} style={activeOutline(0)}>VISUALISE</a>
                 <a className={style.button} onClick={() => click(1)} style={activeOutline(1)}>DEVICE</a>
                 <a className={style.button} onClick={() => click(2)} style={activeOutline(2)}>SENSORS</a>
+                <a className={style.button} onClick={() => click(3)} style={activeOutline(3)}>HISTORY</a>
                 { fullscreen ? null : <a className={style.button} onClick={() => click(3)} style={activeOutline(3)}>MONITOR</a> }
             </p>
         </div>
