652f5a43d8e32ab2a76e5900d8d92fd8da511615
Yuri Sidorov <sidoroyuri@myvuw.ac.nz>
2023-05-18 12:24:52 +1200

    fix(services): return correct types from csv

--- software/device/webapp/src/services/watershed.js
+++ software/device/webapp/src/services/watershed.js
@@ -1,7 +1,7 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 
-export class Watershed {
+class Watershed {
     constructor(filename) {
         this.filename = filename;
     }
@@ -10,12 +10,24 @@ export class Watershed {
         const results = [];
         fs.createReadStream(this.filename)
             .pipe(csv({}))
-            .on('data', (data) => results.push(data))
+            .on('data', (data) => {
+                if (data.Maxim_DS3231_Temp != 'nan') {
+                    data.Maxim_DS3231_Temp = +data.Maxim_DS3231_Temp;
+                }
+                if (data.TBRain_5Min != 'nan') {
+                    data.TBRain_5Min = +data.TBRain_5Min;
+                }
+                results.push(data);
+            })
             .on('end', () => {
                 console.log(results);
             });
         return results;
     }
+}
+
+module.exports = {
+    Watershed
 }
 
 const data = new Watershed("../VUW_Test_TimeSeriesResults.csv");
