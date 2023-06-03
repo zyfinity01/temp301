203b27322d509268e2b550e676bd16d062139d92
Yuri Sidorov <sidoroyuri@myvuw.ac.nz>
2023-06-03 14:28:45 +1200

    chore(webapp): remove unnecessary code from the util component

--- software/device/webapp/src/util/apiClient.ts
+++ software/device/webapp/src/util/apiClient.ts
@@ -13,9 +13,9 @@
 // You should have received a copy of the GNU General Public License
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import {createContext} from "preact";
-import {MockNotyfContext} from "./notyfContext";
-import {Notyf} from "notyf";
+import { createContext } from "preact";
+import { MockNotyfContext } from "./notyfContext";
+import { Notyf } from "notyf";
 
 export const fetchApiContext = createContext(
     () => {}
